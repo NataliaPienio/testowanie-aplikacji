9eb1852f17ecca88a1771d596d0917cf4ca5d675
NataliaPienio <90185181+NataliaPienio@users.noreply.github.com>
2023-10-02 15:33:57 +0200

    Add files via upload

---
+++ 02_10/index.js
@@ -0,0 +1,50 @@
+const test = require('node:test');
+const assert = require('assert');
+
+function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+  
+    if (b > a) {
+      var temp = a;
+      a = b;
+      b = temp; 
+    }
+  
+    while (true) {
+      a %= b;
+      if (a === 0) { return b; }
+      b %= a;
+      if (b === 0) { return a; }
+    }
+  }
+
+  test('testing GCD',(t)=>{
+
+    let a = 10;
+    let b = 20;
+    let expectedResult = 10;
+
+    let result = gcd(a,b);
+
+    assert.strictEqual(result, expectedResult);
+  });
+
+ function potega(a,b){
+    let x=a;
+     while(b>1){
+        a=a*x;
+        b--;
+     }
+     return a;
+ }
+ test('testing potega',(t)=>{
+
+    let a = 2;
+    let b = 10;
+    let expectedResult2 = 1024;
+
+    let result2 = potega(a,b);
+
+    assert.strictEqual(result2, expectedResult2);
+  });
